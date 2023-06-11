e9fc990eb985a515c4d5b76837a6cb4d69aaf8ad
BorisSoaked <borisrunfast@gmail.com>
2023-06-11 18:04:33 +0200

    drawing is working

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,81 +1,59 @@
-import React, {useRef, useEffect} from 'react'
-
-
+import React, {useRef, useEffect, useState} from 'react'
 
 function Canvas() {
     const canvasRef = useRef(null)
-    const mouse = []
-    const stroke = []
-    
-    function renderFrame() {
+    const ctxRef = useRef(null)
+    const [isDrawing, setIsDrawing] = useState(false)
+
+    useEffect(() => {
         const canvas = canvasRef.current
         canvas.width = window.innerWidth * 0.40
         canvas.height = window.innerHeight  * 0.40
         const ctx = canvas.getContext('2d')
-        const canvasPosition = canvas.getBoundingClientRect()
-        // Bacground black full width and height
-        ctx.fillStyle = "#333"
+        ctx.fillStyle = '#333'
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        ctx.lineCap = 'round'
+        ctx.strokeStyle = '#222'
+        ctx.lineWidth = 5
+        ctxRef.current = ctx
+    }, [])
+    
 
-        let isDrawing = false
+    function startDraw({nativeEvent}) {
+        
+        const {offsetX, offsetY} = nativeEvent
+        ctxRef.current.beginPath()
+        ctxRef.current.moveTo(offsetX, offsetY)
+        setIsDrawing(true)
+        console.log(isDrawing)
+    }
 
-        class pen {
-            constructor() {
-                
-            }
-            draw(x, y) {
-                ctx.fillStyle = '#111111'
-                ctx.lineWidth = 3
-                ctx.beginPath()
-                ctx.moveTo(mouse[0]?.x, mouse[0]?.y)
+    function endDraw() {
+        ctxRef.current.closePath()
+        setIsDrawing(false)
+        console.log(isDrawing)
+    }
 
-                for (let i = 0; i < mouse.length; i++) {
-                    ctx.lineTo(mouse[i]?.x, mouse[i]?.y)
-                }
-                ctx.stroke()
-            }
-
+    function draw({nativeEvent}) {
+        if (!isDrawing) {
+            return
         }
-        
-        canvas.addEventListener('mousedown', () => {
-            isDrawing = true
-        })
-        canvas.addEventListener('mouseup', () => {
-            isDrawing = false
-            stroke.push(new pen()) 
-        })
-
-
-        canvas.addEventListener('mousemove', (e) => {
-            
-            if (isDrawing && mouse[mouse.length - 1]?.x !== e.clientX - canvasPosition.left && mouse[mouse.length - 1]?.y !== e.clientY - canvasPosition.top) {
-                mouse.push({x: e.clientX - canvasPosition.left, y: e.clientY - canvasPosition.top})
-            }
-            
-        }) 
-        
-        
-
-        for (let i = 0; i < stroke.length; i++) {
-            stroke[i].draw(mouse[mouse.length - 1]?.x, mouse[mouse.length - 1]?.y)
-        }
-        
+        const {offsetX, offsetY} = nativeEvent
+        ctxRef.current.lineTo(offsetX, offsetY)
+        ctxRef.current.stroke()
+        console.log(isDrawing)
     }
 
 
-    function tick() {
-        if (!canvasRef.current) return
-        renderFrame()
-        requestAnimationFrame(tick)
-      }
-    
-      useEffect(() => {
-        requestAnimationFrame(tick)
-      }, [])
-
     return (
-        <canvas ref={canvasRef}/>
+        <canvas 
+            onMouseDown={startDraw}
+            onMouseMove={draw}
+            onMouseUp={endDraw}
+            ref={canvasRef}
+        />
     )
 }
+
 
 export default Canvas
